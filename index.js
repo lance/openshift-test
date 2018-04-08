f062a6044dd330a46d025f0d93b29cf5f8537921
Lance Ball <lball@redhat.com>
2018-04-08 13:19:51 -0400

    build: add oc commands

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const { createServer } = require('http');
 const { world } = require('world');
 
 const server = createServer((request, response) => response.end(`Hello ${world}`));
-server.listen(8000, _ => console.log('Server listening on port 8080'));
+server.listen(8080, _ => console.log('Server listening on port 8080'));
